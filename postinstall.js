126f25288ca9c4e95dac0c10cc451ed8e222dade
Alex Pryazhnikov <alexpryazhnikov@gmail.com>
2022-10-23 14:08:23 +0700

    chore: test cwd in postinstall script

--- postinstall.js
+++ postinstall.js
@@ -1,4 +1,7 @@
 const childProcess = require('child_process');
+const path = require('path');
 
-childProcess.execSync('npm install');
-childProcess.execSync('npm run build');
+console.log(process.cwd);
+
+// childProcess.execSync('npm install', { cwd: });
+// childProcess.execSync('npm run build');
